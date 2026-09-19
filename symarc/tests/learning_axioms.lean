8a9508789b504828bae0c0ef5b6d346c1857491a
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2026-09-19 17:47:57 +0200

    test: audit foundational dependencies of the accepted learning theorems

---
+++ symarc/tests/learning_axioms.lean
@@ -0,0 +1,17 @@
+import SymArc
+
+#print axioms SymArc.Learning.coherent_iff_fitting
+#print axioms SymArc.Learning.fitting_refinement
+#print axioms SymArc.Learning.conflict_not_constant
+#print axioms SymArc.Learning.condition_preserves_member
+#print axioms SymArc.Learning.predicts_of_subfamily
+#print axioms SymArc.Learning.conditioned_prediction_correct
+#print axioms SymArc.Learning.condition_union
+#print axioms SymArc.Learning.independent_totality_iff
+#print axioms SymArc.Learning.attainable_operation_iff
+#print axioms SymArc.Learning.projection_can_invent_program
+#print axioms SymArc.Learning.fits_orElse
+#print axioms SymArc.Learning.unseen_fallback
+#print axioms SymArc.Learning.ThreePoint.first_fits
+#print axioms SymArc.Learning.ThreePoint.last_fits
+#print axioms SymArc.Learning.ThreePoint.no_common_coherent_coarsening
